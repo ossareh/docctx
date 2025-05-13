2a3ff65197f437238250a4bc2b3c2984291a6fef
P. Michael Ossareh <ossareh@gmail.com>
2025-05-13 16:40:33 -0500

    feat: skel mcp server

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from docctx!")
+from mcp.server.fastmcp import FastMCP
+
+# Initialize FastMCP server
+mcp = FastMCP("docctx")
+
+
+@mcp.tool()
+async def get_docs(name: str) -> str:
+    """Get documents for the specified module
+
+    Args:
+        name: of the library you want docs for
+    """
+    return "\n---\n".join(["foo", "bar", name])
 
 
 if __name__ == "__main__":
-    main()
+    mcp.run(transport="stdio")
