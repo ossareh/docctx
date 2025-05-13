02ac4753bab3e8bf1cac6199a199fa8fb106fc6f
P. Michael Ossareh <ossareh@gmail.com>
2025-05-13 15:42:14 -0500

    feat: initial commit

---
+++ devenv.nix
@@ -0,0 +1,38 @@
+{pkgs, ...}: {
+  # https://devenv.sh/packages/
+  packages = with pkgs; [alejandra nixd nil];
+
+  # https://devenv.sh/languages/
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks.hooks.alejandra.enable = true;
+  git-hooks.hooks.black.enable = true;
+  git-hooks.hooks.commitizen.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from docctx!")
+
+
+if __name__ == "__main__":
+    main()
